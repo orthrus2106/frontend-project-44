09e8469f62c39032f5aa0ccd31b3e0e0ae49c560
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-02-09 17:22:54 +0200

    Added new game "Is number even?"

---
+++ src/game-even.js
@@ -0,0 +1,38 @@
+import readlineSync from 'readline-sync';
+
+const getRandomNumber = () => {
+  const min = Math.ceil(1);
+  const max = Math.floor(100);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const isEven = () => {
+  let counter = 3;
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  while (counter > 0) {
+    let randomNumber = getRandomNumber();
+    let answer = readlineSync.question(
+      `Question: ${randomNumber}\nYour answer: `
+    );
+    if (
+      (randomNumber % 2 === 0 && answer === 'yes') ||
+      (randomNumber % 2 !== 0 && answer === 'no')
+    ) {
+      console.log('Correct!');
+      counter -= 1;
+    } else {
+      console.log(
+        `'${answer}' is wrong answer ;(. Correct answer was '${
+          randomNumber % 2 === 0 ? 'yes' : 'no'
+        }'.`
+      );
+      return;
+    }
+    randomNumber = getRandomNumber();
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
