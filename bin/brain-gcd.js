efd2937f3fd9a321585b32c489164ee8ffff7a22
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-02-11 20:06:09 +0200

    Added new game Find GCD

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainGCD from '../src/games/game-gcd.js';
+
+brainGCD();
---
+++ src/games/game-gcd.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import { runGame } from '../index.js';
+import { getGCD } from '../assets/getgcd.js';
+
+const gameRule = 'Find the greatest common divisor of given numbers.';
+
+const generateBrainGCD = () => {
+  const num1 = _.random(0, 10);
+  const num2 = _.random(0, 10);
+  const question = `${num1} ${num2}`;
+  const answer = getGCD(num1, num2).toString();
+  return [question, answer];
+};
+
+export default () => runGame(generateBrainGCD, gameRule);
