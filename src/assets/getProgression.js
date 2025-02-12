68c1702cff05187bc03f4a6380df36fb572ef9ad
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-02-12 14:53:03 +0200

    fixed progression game

--- src/assets/getProgression.js
+++ src/assets/getProgression.js
@@ -4,8 +4,8 @@ export const getProgression = () => {
   const ary = [];
   const start = _.random(5, 15);
   const step = _.random(2, 7);
-  const end = start + _.random(20, 35);
-  for (let i = 0; start + i * step < end; i += 1) {
+  const length = _.random(6, 15);
+  for (let i = 0; i < length; i += 1) {
     ary.push(start + i * step);
   }
   return ary;
