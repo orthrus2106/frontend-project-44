f8c6f78cad3c3b13797de7d67cd7e2c37775009f
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-02-12 14:58:24 +0200

    fixed game Is Prime Number

--- src/assets/getPrime.js
+++ src/assets/getPrime.js
@@ -1,5 +1,7 @@
 export const getPrime = (n) => {
   let i = 2;
+  if (n < 2) return false;
+  if (n === 2) return true;
   if (n % 2 === 0) return false;
   const limit = Math.sqrt(n);
   while (i <= limit) {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export const runGame = (gameEngine, gameDescripton) => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n${gameDescripton}`);
   const gameRounds = 3;
   for (let i = 0; i < gameRounds; i += 1) {
